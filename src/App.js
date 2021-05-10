10681c566420fee70616ccfd39aa5dad567dcc70
Arseniy <pogosov-91@mail.ru>
2021-05-10 16:17:52 +0300

    design header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <div className="todo">body</div>
+      <div className="header">
+        <ul>
+          <li>Работы</li>
+          <li>Запись</li>
+          <li>Цены</li>
+        </ul>
+      </div>
+      <div className="content">
+        <img
+          src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="pic"></img>
+        <img
+          src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="pic"></img>
+        <img
+          src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="pic"></img>
+      </div>
     </div>
   );
 }
