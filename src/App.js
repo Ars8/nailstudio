70dd6b4982cc89bfc21c88f4e7d4f3cdc3b245ea
Arseniy <pogosov-91@mail.ru>
2021-05-14 18:21:03 +0300

    create header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import { Header } from './component';
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <ul>
-          <li>Работы</li>
-          <li>Запись</li>
-          <li>Цены</li>
-        </ul>
-      </div>
+      <Header />
       <div className="content">
         <img
           src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
---
+++ src/component/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="header">
+      <ul>
+        <li>Работы</li>
+        <li>Запись</li>
+        <li>Цены</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
