204da48659f8ef47ac55cda275b510419797d7f1
Arseniy <pogosov-91@mail.ru>
2021-05-15 13:19:32 +0300

    portfolio component create

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { Header } from './component';
+import Portfolio from './component/Portfolio';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="content">
-        <img
-          src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-          alt="pic"></img>
-        <img
-          src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-          alt="pic"></img>
-        <img
-          src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-          alt="pic"></img>
-      </div>
+      <Portfolio />
     </div>
   );
 }
---
+++ src/component/Portfolio/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './Portfolio.scss';
+
+function Portfolio() {
+  return (
+    <div className="portfolio">
+      <img
+        src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        alt="pic"></img>
+      <img
+        src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        alt="pic"></img>
+      <img
+        src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        alt="pic"></img>
+      <img
+        src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        alt="pic"></img>
+      <img
+        src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        alt="pic"></img>
+      <img
+        src="https://images.unsplash.com/photo-1519014816548-bf5fe059798b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        alt="pic"></img>
+    </div>
+  );
+}
+
+export default Portfolio;
