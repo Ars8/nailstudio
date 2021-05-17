dd4b8fd71333dff82e5da04ec7ebbdf2a7a4a598
Arseniy <pogosov-91@mail.ru>
2021-05-17 17:50:28 +0300

    Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { Header, Portfolio, Appointment, Price } from './component';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Portfolio/>
-      <Appointment/>
-      <Price/>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Portfolio />
+        </Route>
+        <Route path="/appoint">
+          <Appointment />
+        </Route>
+        <Route path="/price">
+          <Price />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.scss';
 
@@ -6,9 +7,15 @@ function Header() {
   return (
     <div className="header">
       <ul>
-        <li>Работы</li>
-        <li>Запись</li>
-        <li>Цены</li>
+        <li>
+          <Link to="/">Работы</Link>
+        </li>
+        <li>
+          <Link to="/appoint">Запись</Link>
+        </li>
+        <li>
+          <Link to="/price">Цены</Link>
+        </li>
       </ul>
     </div>
   );
