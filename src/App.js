5d9b90c1bb4ad2962555e3bde93db7ff54b6e0f3
Ars8 <pogosov-91@mail.ru>
2021-08-10 18:30:23 +0300

    new nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import { Header, Portfolio, Appointment, Price } from './component';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
     <div className="App">
       <Header className="header" />
       <div className="content">
-        <Switch>
-          <Route exact path="/">
-            <Portfolio />
-          </Route>
-          <Route path="/appoint">
-            <Appointment />
-          </Route>
-          <Route path="/price">
-            <Price />
-          </Route>
-        </Switch>
+        <Router>
+          <MainNavigation />
+          <main>
+            <Switch>
+              <Route path="/" exact>
+                <Portfolio />
+              </Route>
+              <Route path="/appoint">
+                <Appointment />
+              </Route>
+              <Route path="/price">
+                <Price />
+              </Route>
+              <Redirect to="/" />
+            </Switch>
+          </main>
+        </Router>
       </div>
     </div>
   );
